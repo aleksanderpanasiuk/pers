464d1b7a44fa3b7fa533031e08e1213264371202
aleksanderpanasiuk <olekp249@gmail.com>
2023-10-28 15:01:08 +0200

    Removed copied indices from cube

--- src/Objects/Cube.h
+++ src/Objects/Cube.h
@@ -11,30 +11,6 @@ public:
 
 protected:
 	float sideLength;
-
-	std::vector <glm::vec3> verticesPosition;
-	std::vector <Vertex> Vertices;
-
-	std::vector <GLuint> Indices =
-	{
-		0, 1, 2, // bottom
-		0, 2, 3,
-
-		4, 5, 6, // top
-		4, 6, 7,
-
-		8, 9, 11, // front
-		8, 11, 10,
-
-		13, 12, 14, // back
-		13, 14, 15,
-
-		16, 17, 19, // left
-		16, 18, 19,
-
-		20, 21, 23, // right
-		20, 22, 23
-	};
 };
 
 #endif
