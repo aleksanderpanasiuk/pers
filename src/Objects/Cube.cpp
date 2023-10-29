a839c898676eb06cc66776b55d3d8cacd5ddd535
aleksanderpanasiuk <olekp249@gmail.com>
2023-10-29 10:28:12 +0100

    Removed unnecessary variable duplication

--- src/Objects/Cube.cpp
+++ src/Objects/Cube.cpp
@@ -4,5 +4,4 @@ Cube::Cube(Shader& shader, glm::vec3 Position, float sideLength, glm::vec3 Color
 	Rectangle(shader, Position, glm::vec3(sideLength, sideLength, sideLength), Color)
 {
 	Cube::sideLength = sideLength;
-	Rectangle::Color = glm::vec3(0.5f, 0.2f, 0.2f);
 }
