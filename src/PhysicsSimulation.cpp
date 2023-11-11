d810c73e24ba438fc996acaeba1220fbbf27fedc
aleksanderpanasiuk <olekp249@gmail.com>
2023-11-11 08:40:22 +0100

    Implemented base physics simulation functionality

---
+++ src/PhysicsSimulation.cpp
@@ -0,0 +1,16 @@
+#include "PhysicsSimulation.h"
+
+void PhysicsSimulation::simulate()
+{
+
+}
+
+std::vector<RigidBody> PhysicsSimulation::getRigidBodies()
+{
+	return rigidBodies;
+}
+
+void PhysicsSimulation::addRigidBody(RigidBody rigidBody)
+{
+	rigidBodies.push_back(rigidBody);
+}
