da8f92e315b49bc3c636bac5ed34c9f99c799c4c
aleksanderpanasiuk <olekp249@gmail.com>
2023-11-11 08:32:57 +0100

    Added vector of rigid bodies

--- src/PhysicsSimulation.h
+++ src/PhysicsSimulation.h
@@ -1,10 +1,17 @@
 #ifndef PHYSICS_SIMULATION_CLASS_H
 #define PHYSICS_SIMULATION_CLASS_H
 
+#include "Objects/RigidBody.h"
+
 class PhysicsSimulation
 {
 public:
 	void simulate();
+	std::vector<RigidBody> getRigidBodies();
+	void addRigidBody(RigidBody rigidBody);
+
+private:
+	std::vector<RigidBody> rigidBodies;
 };
 
 #endif // !PHYSICS_SIMULATION_CLASS_H
