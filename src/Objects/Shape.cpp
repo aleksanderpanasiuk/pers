0bd09ad48398d0833c7946a48aa39dffbe03ba20
aleksanderpanasiuk <olekp249@gmail.com>
2023-11-11 12:33:53 +0100

    Implemented data selection for cube

---
+++ src/Objects/Shape.cpp
@@ -0,0 +1,100 @@
+#include "Shape.h"
+
+Shape::Shape(RigidType type)
+{
+	Shape::type = type;
+
+	switch (type)
+	{
+	case RigidCube:
+		setDataCube();
+		break;
+	case RigidSphere:
+		break;
+	case RigidPlane:
+		break;
+	default:
+		break;
+	}
+}
+
+void Shape::setDataCube()
+{
+	glm::vec3 Size = glm::vec3(1.0f, 1.0f, 1.0f);
+
+	/*
+		   +----------------------------------+
+		  / | 7								/ | 6
+		 /  							   /  |
+		/   |							  /   |
+	   /    							 /    |
+	  +---------------------------------+     |
+	  |	3	|							| 2   |
+	  |									|	  |
+	  |		|							|	  |
+	  |									|	  |
+	  |		|							|	  |
+	  |									|	  |
+	  |		|							|	  |
+	  |		+  --  --  --  --  --  --  -| -- +
+	  |      4  						|	 / 5
+	  |   /								|   /
+	  |  								|  /
+	  | /								| /
+	  +---------------------------------+
+	   0								 1
+
+		y
+		^
+		|  z
+		| /
+		|/
+		#----- > x
+	*/
+
+	verticesPosition = {
+		glm::vec3(-(Size.x / 2),  -(Size.y / 2),   -(Size.z / 2)),
+		glm::vec3((Size.x / 2),  -(Size.y / 2),   -(Size.z / 2)),
+		glm::vec3((Size.x / 2),   (Size.y / 2),   -(Size.z / 2)),
+		glm::vec3(-(Size.x / 2),   (Size.y / 2),   -(Size.z / 2)),
+		glm::vec3(-(Size.x / 2),  -(Size.y / 2),   +(Size.z / 2)),
+		glm::vec3((Size.x / 2),  -(Size.y / 2),   +(Size.z / 2)),
+		glm::vec3((Size.x / 2),   (Size.y / 2),   +(Size.z / 2)),
+		glm::vec3(-(Size.x / 2),   (Size.y / 2),   +(Size.z / 2))
+	};
+
+	Vertices =
+	{ //     COORDINATES          //   COLORS   //         NORMALS           // TEXTURE COORDINATES
+		Vertex{verticesPosition[0],    Color, glm::vec3(0.0f, 0.0f, -1.0f), glm::vec2(0.0f, 0.0f)}, // 0 front
+		Vertex{verticesPosition[1],    Color, glm::vec3(0.0f, 0.0f, -1.0f), glm::vec2(0.0f, 0.0f)},
+		Vertex{verticesPosition[2],    Color, glm::vec3(0.0f, 0.0f, -1.0f), glm::vec2(0.0f, 0.0f)},
+		Vertex{verticesPosition[3],    Color, glm::vec3(0.0f, 0.0f, -1.0f), glm::vec2(0.0f, 0.0f)},
+
+		Vertex{verticesPosition[4],    Color, glm::vec3(0.0f, 0.0f, 1.0f), glm::vec2(0.0f, 0.0f)}, // 4 back
+		Vertex{verticesPosition[5],    Color, glm::vec3(0.0f, 0.0f, 1.0f), glm::vec2(0.0f, 0.0f)},
+		Vertex{verticesPosition[6],    Color, glm::vec3(0.0f, 0.0f, 1.0f), glm::vec2(0.0f, 0.0f)},
+		Vertex{verticesPosition[7],    Color, glm::vec3(0.0f, 0.0f, 1.0f), glm::vec2(0.0f, 0.0f)},
+
+		Vertex{verticesPosition[0],    Color, glm::vec3(0.0f, -1.0f, 0.0f), glm::vec2(0.0f, 0.0f)}, // 8 bottom
+		Vertex{verticesPosition[1],    Color, glm::vec3(0.0f, -1.0f, 0.0f), glm::vec2(0.0f, 0.0f)},
+		Vertex{verticesPosition[4],    Color, glm::vec3(0.0f, -1.0f, 0.0f), glm::vec2(0.0f, 0.0f)},
+		Vertex{verticesPosition[5],    Color, glm::vec3(0.0f, -1.0f, 0.0f), glm::vec2(0.0f, 0.0f)},
+
+		Vertex{verticesPosition[2],    Color, glm::vec3(0.0f, 1.0f, 0.0f), glm::vec2(0.0f, 0.0f)}, // 12 top
+		Vertex{verticesPosition[3],    Color, glm::vec3(0.0f, 1.0f, 0.0f), glm::vec2(0.0f, 0.0f)},
+		Vertex{verticesPosition[6],    Color, glm::vec3(0.0f, 1.0f, 0.0f), glm::vec2(0.0f, 0.0f)},
+		Vertex{verticesPosition[7],    Color, glm::vec3(0.0f, 1.0f, 0.0f), glm::vec2(0.0f, 0.0f)},
+
+		Vertex{verticesPosition[0],    Color, glm::vec3(-1.0f, 0.0f, 0.0f), glm::vec2(0.0f, 0.0f)}, // 16 left
+		Vertex{verticesPosition[3],    Color, glm::vec3(-1.0f, 0.0f, 0.0f), glm::vec2(0.0f, 0.0f)},
+		Vertex{verticesPosition[4],    Color, glm::vec3(-1.0f, 0.0f, 0.0f), glm::vec2(0.0f, 0.0f)},
+		Vertex{verticesPosition[7],    Color, glm::vec3(-1.0f, 0.0f, 0.0f), glm::vec2(0.0f, 0.0f)},
+
+		Vertex{verticesPosition[1],    Color, glm::vec3(1.0f, 0.0f, 0.0f), glm::vec2(0.0f, 0.0f)}, // 20 right
+		Vertex{verticesPosition[2],    Color, glm::vec3(1.0f, 0.0f, 0.0f), glm::vec2(0.0f, 0.0f)},
+		Vertex{verticesPosition[5],    Color, glm::vec3(1.0f, 0.0f, 0.0f), glm::vec2(0.0f, 0.0f)},
+		Vertex{verticesPosition[6],    Color, glm::vec3(1.0f, 0.0f, 0.0f), glm::vec2(0.0f, 0.0f)}
+	};
+
+	mesh.setData(Vertices, Indices);
+}
--- src/Objects/Shape.h
+++ src/Objects/Shape.h
@@ -13,15 +13,17 @@ public:
 
 private:
 	Mesh mesh;
+	RigidType type;
 
 
 	glm::vec3 Color = glm::vec3(0.0f, 0.0f, 0.0f);
-	
+
 
 	std::vector <glm::vec3> verticesPosition;
 	std::vector <Vertex> Vertices;
-
 	std::vector <GLuint> Indices;
+
+	void setDataCube();
 };
 
 #endif // !SHAPE_CLASS_H
