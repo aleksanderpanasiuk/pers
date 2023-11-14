d889fd26440629c4f9bbde7f3a89f95b713c5f7c
aleksanderpanasiuk <olekp249@gmail.com>
2023-11-14 18:48:21 +0100

    Implemented basic object funcionality

--- src/Objects/Object.cpp
+++ src/Objects/Object.cpp
@@ -1,11 +1,34 @@
 #include "Object.h"
 
-void Object::Update()
+
+Object::Object(GraphicsComponent& graphicsComponent, RigidBody& rigidBody)
+	: graphicsComponent(graphicsComponent),
+	rigidBody(rigidBody)
 {
 
 }
 
+void Object::Update(float deltaTime)
+{
+	rigidBody.Move(deltaTime);
+}
+
 void Object::Render(Shader& shader, Camera& camera)
 {
-	graphicsComponent.Draw(shader, camera)
+	graphicsComponent.Draw(
+		shader, camera,
+		rigidBody.getPosition(),
+		rigidBody.getOrientation(),
+		rigidBody.getScale()
+	);
+}
+
+GraphicsComponent& Object::getGraphicsComponent()
+{
+	return graphicsComponent;
+}
+
+RigidBody& Object::getRigidBody()
+{
+	return rigidBody;
 }
--- src/Objects/Object.h
+++ src/Objects/Object.h
@@ -7,11 +7,16 @@
 class Object
 {
 public:
+	Object(GraphicsComponent& graphicsComponent, RigidBody& rigidBody);
+	void Update(float deltaTime);
+	void Render(Shader& shader, Camera& camera);
+
+	GraphicsComponent& getGraphicsComponent();
+	RigidBody& getRigidBody();
+
+private:
 	GraphicsComponent graphicsComponent;
 	RigidBody rigidBody;
-
-	void Update();
-	void Render(Shader& shader, Camera& camera);
 };
 
 #endif // !OBJECT_CLASS_H
