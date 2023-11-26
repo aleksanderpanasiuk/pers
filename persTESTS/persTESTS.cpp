791c2110033bffeda65969c317cbea799a2fd7ea
aleksanderpanasiuk <olekp249@gmail.com>
2023-11-26 17:56:04 +0100

    added test setup files

---
+++ persTESTS/persTESTS.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace persTESTS
+{
+	TEST_CLASS(persTESTS)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
