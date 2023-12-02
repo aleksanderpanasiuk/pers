b78a1061cd6276be4c00674b5e212bb726149bf3
aleksanderpanasiuk <olekp249@gmail.com>
2023-12-02 21:34:22 +0100

    Added basic inspector window class

---
+++ src/UIElements/InspectorWindow.h
@@ -0,0 +1,14 @@
+#ifndef INSPECTOR_WINDOW_H
+#define INSPECTOR_WINDOW_H
+
+#include "Window.h"
+
+class InspectorWindow : public Window
+{
+public:
+	void Display(Object& SelectedObject);
+};
+
+
+#endif // !INSPECTOR_WINDOW_H
+
